f58698bafa70ca6a48a894657051044a6dd6d2e9
ashviniu <135208748+ashviniu@users.noreply.github.com>
2025-05-16 12:52:11 +0530

    Add files via upload

---
+++ SQL_Exports/NetExamV5_Staging/2025-05-16/User_Change_ChipPoint_log.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User_Change_ChipPoint_log] (
+    [UserID] char(32) NULL,
+    [lifeTimePoints] int NULL,
+    [currentPoints] int NULL,
+    [chips] int NULL,
+    [DateStamp] datetime NULL,
+    [Status] varchar(1000) NULL,
+    [imerged] int NULL
+);
